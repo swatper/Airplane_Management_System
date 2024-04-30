211905436b7a69f37fdb042fe0a145dffabbc598
swatper <67682393+swatper@users.noreply.github.com>
2024-04-30 15:03:43 +0900

    Test
    asasas

---
+++ Airplane_Management_System/src/main/java/cse/airplane_management_system/Airplane_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cse.airplane_management_system;
+
+/**
+ *
+ * @author 박상현
+ */
+public class Airplane_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
