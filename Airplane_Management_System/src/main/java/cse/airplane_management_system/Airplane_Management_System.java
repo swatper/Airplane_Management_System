8174e115889cc9c38bdd33d3e3958617deec69a2
swatper <67682393+swatper@users.noreply.github.com>
2024-05-21 16:04:04 +0900

    병합 코드 수정
    시스템간 연동

--- Airplane_Management_System/src/main/java/cse/airplane_management_system/Airplane_Management_System.java
+++ Airplane_Management_System/src/main/java/cse/airplane_management_system/Airplane_Management_System.java
@@ -1,5 +1,6 @@
 package cse.airplane_management_system;
 
+import cse.airplane_management_system.AirPlaneSystem.AirPlaneSystem;
 import cse.airplane_management_system.LoginSystem.LoginSystem;
 import cse.airplane_management_system.LoginSystem.User;
 import java.io.IOException;
@@ -11,6 +12,7 @@ public class Airplane_Management_System {
 
     public User LoginUser;
     public LoginSystem loginSystem;
+    public AirPlaneSystem airPlaneSystem;
 
     /*
     public AirplaneSystem  airplaneSystem;
@@ -24,19 +26,24 @@ public class Airplane_Management_System {
     ;
     
     //각 시스템 객체 생성
-    public void Init() {
+    public void Init() throws IOException {
         loginSystem = LoginSystem.GetSystem();
+        airPlaneSystem = new AirPlaneSystem();
+        
         /*
-        airplaneSystem = new AiplaneSystem();
-        reservationSystem = new ReservationSystem();
         reportSystem = new ReportSystem();
          */
     }
 
     //로그인 시스템 실행
     public void RunLogin() throws IOException {
+        //로그인 시스템
         loginSystem.Init();
         loginSystem.RunSystem();
         LoginUser = loginSystem.GetLoginUser();
+        
+        //비행기 관리 시스템
+        airPlaneSystem.RunSystem(LoginUser);
+        
     }
 }
