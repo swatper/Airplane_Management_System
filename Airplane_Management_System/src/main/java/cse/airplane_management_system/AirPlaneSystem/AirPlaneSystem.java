d2985e74cc7ca6db5b5c71231197f9606c186f5e
amy5664 <amy5664@naver.com>
2024-05-21 16:25:51 +0900

    간단한 줄바꿈
    메뉴 띄우기, 칸 나누기

--- Airplane_Management_System/src/main/java/cse/airplane_management_system/AirPlaneSystem/AirPlaneSystem.java
+++ Airplane_Management_System/src/main/java/cse/airplane_management_system/AirPlaneSystem/AirPlaneSystem.java
@@ -81,7 +81,8 @@ public class AirPlaneSystem {
     Scanner scanner = new Scanner(System.in);
 
     while (true) {
-        System.out.println("메인 메뉴:");
+        System.out.println("======================================");
+        System.out.println("메인 메뉴");
         System.out.println("1. 모든 항공편 조회");
         System.out.println("2. 관리자 메뉴");
         System.out.println("3. 종료");
@@ -113,7 +114,8 @@ public class AirPlaneSystem {
 
 private void runAdminMenu(Scanner scanner) throws IOException {
     while (true) {
-        System.out.println("관리자 메뉴:");
+        System.out.println("======================================");
+        System.out.println("관리자 메뉴");
         System.out.println("1. 새로운 항공편 추가");
         System.out.println("2. 항공편 수정");
         System.out.println("3. 항공편 삭제");
